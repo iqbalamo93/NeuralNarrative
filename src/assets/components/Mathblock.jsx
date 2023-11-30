1e4c6a00384cd67b6dc0bdc00c5ec3b7faa3f377
iqbal Singh <iqbalamo93@gmail.com>
2023-11-30 14:45:18 +0000

    first framework draft_2

--- src/assets/components/Mathblock.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BlockMath, InlineMath } from 'react-katex';
-import 'katex/dist/katex.min.css';
-
-const LatexMath = ({ equation, inline = false }) => {
-  return inline ? <InlineMath math={equation} /> : <BlockMath math={equation} />;
-};
-
-export default LatexMath;
