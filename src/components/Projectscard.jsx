f3292af5bfb02fefe50b17a632cf4fd2ee10eb28
iqbal Singh <iqbalamo93@gmail.com>
2024-01-09 06:54:17 +0000

    'img'

--- src/components/Projectscard.jsx
+++ src/components/Projectscard.jsx
@@ -1,6 +1,8 @@
 import './Projectscard.css'
 
+
 export default function ProjectsCard() {
+    img_var = "https://image.lexica.art/full_webp/06bb677b-5666-4f10-b941-150b3e89118f"
     return (
     <div className='popular-articles' id='project'>
         
@@ -53,7 +55,7 @@ export default function ProjectsCard() {
 
                 <div class="card-author">
                 <a class="author-avatar" href="#">
-                    <img src="./src/components/img.png" />
+                    <img src={img_var} />
                 </a>
                 <svg class="half-circle" viewBox="0 0 106 57">
                     <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
@@ -81,7 +83,7 @@ export default function ProjectsCard() {
 
                 <div class="card-author">
                 <a class="author-avatar" href="#">
-                    <img src="img.png" />
+                    <img src={img_var} />
                 </a>
                 <svg class="half-circle" viewBox="0 0 106 57">
                     <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
