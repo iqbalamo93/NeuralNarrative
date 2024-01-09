5ae8a79c80a9e6ebfdae22400559e2c5e9e8ee29
iqbal Singh <iqbalamo93@gmail.com>
2024-01-09 06:33:30 +0000

    'img'

--- src/components/Projectscard.jsx
+++ src/components/Projectscard.jsx
@@ -23,7 +23,7 @@ export default function ProjectsCard() {
 
                 <div class="card-author">
                 <a class="author-avatar" href="#">
-                    <img src="img.png" />
+                    <img src="./src/components/img.png" />
                 </a>
                 <svg class="half-circle" viewBox="0 0 106 57">
                     <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
@@ -53,7 +53,7 @@ export default function ProjectsCard() {
 
                 <div class="card-author">
                 <a class="author-avatar" href="#">
-                    <img src="img.png" />
+                    <img src="./src/components/img.png" />
                 </a>
                 <svg class="half-circle" viewBox="0 0 106 57">
                     <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
