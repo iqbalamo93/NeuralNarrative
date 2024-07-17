45f258f3102255a04c3bc643b302f4113fd19354
iqbal Singh <iqbalamo93@gmail.com>
2024-07-18 01:03:05 +0530

    'suctured'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-// import starlight from 'starlight-patch';
 import remarkMath from 'remark-math';
 import rehypeMathJax from 'rehype-mathjax';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 // import  { Props } from '@astrojs/starlight/props';
-
+import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,41 +14,60 @@ export default defineConfig({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathJax]
   },
-  integrations: [starlight({
-    title: 'Home',
-    social: {
-      github: 'https://github.com/iqbalamo93/NeuralNarrative'
-    },
-    components:{
-
-      Header: './src/components/Header.astro',
-    },
-    sidebar: [{
-      label: 'Blogs',
-      autogenerate: {
-        directory: 'blogs'
-      }
-    }
-    , {
-      label: 'Books Review',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'What is this!',
-        link: '/books-review/intro-reviews/' //will change this in future
-      }]
-    }, {
-      label: 'Element of Neural Networks',
-      autogenerate: {
-        directory: 'elements-of-nn'
-      }
-    }, {
-      label: 'Frameworks FastTrack',
-      autogenerate: {
-        directory: 'framework-fastrack'
-      }
-    }]
-  }), tailwind({
-    applyBaseStyles: false
-  }), react(), tailwind(), markdoc()]
+  integrations: [
+    starlight({
+      plugins: [starlightImageZoom()],
+      title: 'Home',
+      social: {
+        github: 'https://github.com/iqbalamo93/NeuralNarrative'
+      },
+      components: {
+        Header: './src/components/Header.astro',
+      },
+      sidebar: [
+        {
+          label: 'Blogs',
+          items: [
+            'blogs/intro-blog',
+            // Nested stuff
+            {
+              label: 'Transformers',
+              items: ['blogs/transformers/00_intro', 'blogs/transformers/05_transformer',],
+            },
+            {
+              label: 'Attention',
+              items: ['blogs/attention/self_attention',],
+            },
+          ],
+        },
+        {
+          label: 'Books Review',
+          items: [
+            {
+              label: 'What is this!',
+              link: '/books-review/intro-reviews/' // will change this in future
+            }
+          ]
+        },
+        {
+          label: 'Element of Neural Networks',
+          autogenerate: {
+            directory: 'elements-of-nn'
+          }
+        },
+        {
+          label: 'Frameworks FastTrack',
+          autogenerate: {
+            directory: 'framework-fastrack'
+          }
+        }
+      ]
+    }),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    tailwind(),
+    markdoc()
+  ]
 });
